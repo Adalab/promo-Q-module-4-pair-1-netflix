ddd4015b84db99f0d4ebe756790cee8797ae182c
SandMl3 <sandramartinlop@gmail.com>
2022-09-05 11:31:25 +0200

    Add first endpoint and query params

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const dataMovies = require('./data/movies.json');
+const { response } = require('express');
+
 
 // create and config server
 const server = express();
@@ -10,4 +13,18 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', (req, res) => {
+  const genderFilterParam = req.query;
+  // const movies = JSON.parse(dataMovies)
+  console.log(genderFilterParam)
+  const response = {
+    success: true,
+    movies: dataMovies
+    //   .filter(item => item.gender === genderFilterParam.ender)
+    // 
+  };
+ 
+  res.json(response);
 });
